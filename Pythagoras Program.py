8d18268655d9e2bb7d4db7ce99547054f715d1b6
eea11 <emmanuelalaeze@seoulchristian.org>
2015-12-06 16:21:44 +0900

    finished up the program.

--- Pythagoras Program.py
+++ Pythagoras Program.py
@@ -1,57 +1,13 @@
-sides  = int(raw_input("Choose which equation to enter. Choose out of AB=C(1) AC=B(2)  or BC=A(3) "))
-if sides == 1:
-    print "OK"
-    A = int(raw_input("What is the length of side A? "))
-    B = int(raw_input("What is the length of side B? "))
-    # After finding side A and B, I must multiply them to the second power and add to find C.
-    A_2 = A ** 2
-
-    B_2 = B ** 2
-
-    C_2 = A_2 + B_2
-    # Then I must find the sqaure root.
-    C = C_2 ** 0.5
-
-    print "The hypotenuse's length is", C
-
-elif sides  == 2:
-    print "OK"
-    A = int(raw_input("What is the length of side A? "))
-    C = int(raw_input("What is the length of side C? "))
-    # After finding side B and C, I must multiply them to the second power, but instead of adding, I must subtract C^2 by A^2 and the sqaure root to find B.
-    A_2 = A ** 2
-
-    C_2 = C ** 2
-
-    B_2 = C_2 - A_2
-
-    B = B_2 ** 0.5
-
-    print "Side B's length is", B
-elif sides == 3:
-    print "OK"
-    B = int(raw_input("What is the length of side B? "))
-    C = int(raw_input("What is the length of side C? "))
-    # The final equation is exactly like the first page, but you substitute in B^2 for A^2 and C^2 for B^2.
-    B_2 = B ** 2
-
-    C_2 = C ** 2
-
-    A_2 = B_2 + C_2
-    # Then I must find the sqaure root.
-    A = A_2 ** 0.5
-
-    print "Side A's length is", A
-else:
-    print "That is not one of the specified numbers or symbols please choose either 1, 2, or 3."
-# I basically recopied everything in case they printed the wrong number.
-# So I could give another chance.
-    sides  = int(raw_input("Choose which equation to enter. Choose out of AB=C(1) AC=B(2)  or BC=A(3) "))
+def beginning () :
+# This is my introductory sentence.
+    print "Hello! Welcome to my program which can be used to solve for either side A,B, or C. "
+    sides  = int(raw_input("Choose which equation to enter. Choose out of AB=C(1) AC=B(2)  or BC=A(3) or (4) to end the program."))
+    # I have the user to choose which two sides to enter because the equation to solve for different sides is different depending on which to side you want to find out.
     if sides == 1:
         print "OK"
         A = int(raw_input("What is the length of side A? "))
         B = int(raw_input("What is the length of side B? "))
-        # After finding side A and B, I must multiply them to the second power and add to find C.
+        # After finding the length of sides A and B, I must multiply them both to the second power, add, and square root the answer to find the length of side C.
         A_2 = A ** 2
 
         B_2 = B ** 2
@@ -61,11 +17,12 @@ else:
         C = C_2 ** 0.5
 
         print "The hypotenuse's length is", C
-
+        beginning()
     elif sides  == 2:
         print "OK"
         A = int(raw_input("What is the length of side A? "))
         C = int(raw_input("What is the length of side C? "))
+        # This is nearly the same as the first one, but has a few differences.
         # After finding side B and C, I must multiply them to the second power, but instead of adding, I must subtract C^2 by A^2 and the sqaure root to find B.
         A_2 = A ** 2
 
@@ -76,11 +33,13 @@ else:
         B = B_2 ** 0.5
 
         print "Side B's length is", B
+        beginning()
     elif sides == 3:
         print "OK"
         B = int(raw_input("What is the length of side B? "))
         C = int(raw_input("What is the length of side C? "))
         # The final equation is exactly like the first page, but you substitute in B^2 for A^2 and C^2 for B^2.
+        # First, you find the length of sides B and C. Then, you multiply them both to the second power, add the two numbers that you just multiplied to the second power, and find the square root.
         B_2 = B ** 2
 
         C_2 = C ** 2
@@ -90,3 +49,12 @@ else:
         A = A_2 ** 0.5
 
         print "Side A's length is", A
+        beginning()
+    elif sides == 4:
+        loop = 0
+        # This ends the program.
+    else:
+        print "That is not one of the specified numbers or symbols please choose either 1, 2, 3, or 4."
+        print "Please rerun the program."
+        # The user has to restart the program with the correct characters.
+beginning()
